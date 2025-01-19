38c32adeac345dea734e980d32a85dab464d91e9
antheiz <salupacode@gmail.com>
2025-01-19 11:15:11 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+
+class TerminalExtension(Extension):
+    def __init__(self):
+        super().__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        query = event.get_argument() or ""
+        
+        if not query:
+            # Jika tidak ada perintah, tampilkan placeholder
+            return RenderResultListAction([
+                ExtensionResultItem(
+                    icon='images/icon.png',  # Sesuaikan dengan path icon Anda
+                    name='Masukkan perintah terminal',
+                    description='Contoh: ls, pwd, ps aux',
+                    on_enter=RunScriptAction('gnome-terminal')  # Sesuaikan dengan terminal default Anda
+                )
+            ])
+
+        # Jika ada perintah, jalankan di terminal
+        return RenderResultListAction([
+            ExtensionResultItem(
+                icon='images/icon.png',  # Sesuaikan dengan path icon Anda
+                name=f'Jalankan: {query}',
+                description=f'Eksekusi "{query}" di terminal',
+                on_enter=RunScriptAction(f'gnome-terminal -- bash -c "{query}; exec bash"')
+            )
+        ])
+
+if __name__ == '__main__':
+    TerminalExtension().run()
